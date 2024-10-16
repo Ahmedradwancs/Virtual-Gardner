bfe3c39a298db8658685c1e272d140a79f0f3ea9
Alloush95 <mohamad.alloush0090@stud.hkr.se>
2024-10-16 15:38:10 +0200

    created WelcomeScreen class and added an image

--- app/src/main/java/com/example/virtualgardner/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/virtualgardner/ui/screens/WelcomeScreen.kt
@@ -1,20 +1,30 @@
 package com.example.virtualgardner.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.virtualgardner.R
 
 @Composable
@@ -22,71 +32,80 @@ fun WelcomeScreen(
     onSignInClick: () -> Unit,
     onSignUpClick: () -> Unit
 ) {
-    val backgroundImage = painterResource(id = R.drawable.launchingpage)
-    Box(modifier = Modifier.fillMaxSize()) {
-        // Background image
-        Image(
-            painter = backgroundImage,
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-            contentScale = androidx.compose.ui.layout.ContentScale.Crop
-        )
-
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color(0xFFF5F5DC))
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF333333).copy(alpha = 0.5f),
+                        Color(0xFF006400).copy(alpha = 0.4f)
+                    )
+                )
+            )
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceAround
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "The best app for your plants",
-            modifier = Modifier.padding(16.dp),
-            style = MaterialTheme.typography.displayLarge,
-            color = MaterialTheme.colorScheme.onPrimary,
-        )
+        item {
+            Text(
+                text = "Join for Smart Gardening experience",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFFF5F5DC),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 16.dp),
+                textAlign = TextAlign.Center
+            )
 
-        Spacer(modifier = Modifier.height(32.dp))
+            Spacer(modifier = Modifier.height(70.dp))
 
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+            Image(
+                painter = painterResource(id = R.drawable.wlecome),
+                contentDescription = "Welcome to Virtual Gardener",
+                modifier = Modifier
+                    .size(width = 343.dp, height = 376.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
 
-        ) {
+            Spacer(modifier = Modifier.height(35.dp))
 
             Button(
                 onClick = onSignInClick,
-                modifier = Modifier.width(300.dp),
+                modifier = Modifier
+                    .size(width = 280.dp, height = 70.dp)
+                    .padding(bottom = 16.dp),
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0x80B0B0B0), // Set transparent background
-                    contentColor = MaterialTheme.colorScheme.onPrimary // Set text color
-                ),
-
-            )
-            {
-                Text(text = "Sign in")
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Button(
-                onClick = onSignUpClick,
-                modifier = Modifier.width(300.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.Transparent, // Set transparent background
-                    contentColor = MaterialTheme.colorScheme.onPrimary // Set text color
+                    containerColor = Color(0xFF3B2314).copy(alpha = 0.5f) // Button with transparency
                 )
             ) {
-                Text(text = "Create an account")
+                Text("Sign in", fontSize = 24.sp, color = Color(0xFFF5F5DC))
+            }
+
+            Spacer(modifier = Modifier.height(35.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                ClickableText(
+                    text = AnnotatedString("Create an account"),
+                    onClick = { onSignUpClick() },
+                    style = TextStyle(
+                        fontSize = 22.sp,
+                        color = Color(0xFFF5F5DC),
+                        textDecoration = TextDecoration.Underline
+                    )
+                )
             }
         }
-
-
     }
 }
-}
-
 
 @Preview
 @Composable
